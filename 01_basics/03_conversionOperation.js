2847b413b684d08355a83aa230714bf57fb67140
Rohan30020407 <rohansoniblphp@gmail.com>
2026-03-19 15:25:57 +0000

    Conversion Of Datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,7 +19,20 @@ console.log(valueInNumber)//NaN = Not a number.
 // "33abc" => NaN
 // true =>1; false =>0; 
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+let isLoggedIn = "Rohan"
+
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// 1=>true 0=>false 
+// ""=>false
+// "Rohan"=>true
+let somenumber = 79
+
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
