130023e8f23221f2b7c01006689d088540835c5c
Rohan30020407 <rohansoniblphp@gmail.com>
2026-03-19 17:59:46 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,12 +8,12 @@
 // let score = "undefined"
 let score = true
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)//NaN = Not a number.
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)//NaN = Not a number.
 
 // "33"=> 33
 // "33abc" => NaN
@@ -34,5 +34,56 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 let somenumber = 79
 
 let stringnumber = String(somenumber)
-console.log(stringnumber)
-console.log(typeof stringnumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+
+
+// ********************************* Operations **************************************************
+
+let value = 100
+let negValue = -value
+// console.log(negValue)
+
+// console.log(7+7)
+// console.log(7-3)
+// console.log(7*7)
+// console.log(3/7)
+// console.log(3%7)
+// console.log(7**3)
+
+let str1 = "Raja"
+
+let str2 = " Rohan"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + "2" + 2)
+// console.log(1 + 2 + "2")
+
+// jb string pehle to connect honge
+// jb string baad mai to pehle wale connect honge
+
+// console.log((3 + 4) * 5 % 3)
+
+// console.log(true)
+// console.log(+true)
+// console.log(true+)ye error deta hai code rin hi ni hoga
+// console.log(true-)error
+// ye increment wali value deta hai 0 ya 1 ki form mai
+// console.log("")
+// console.log(+"")
+// console.log(""+)error
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 77
+gamecounter++;
+console.log(gamecounter)
+++gamecounter;
+console.log(gamecounter)
