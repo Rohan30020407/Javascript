4e12a039a377a3368e3f22e6b80415686d38cefd
Rohan30020407 <rohansoniblphp@gmail.com>
2026-04-16 11:15:17 +0000

    Manual and Explicit Number Datatype

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,5 +1,6 @@
 const score = 400
 console.log(score)
+// difference between automatically thinking as number and manually typing as number.
 
 const balance = new Number(100)
 console.log(balance);
