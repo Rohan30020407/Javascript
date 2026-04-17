2ae54575dc3ac65710e1e9b15e70e994b850234f
Rohan30020407 <rohansoniblphp@gmail.com>
2026-04-17 11:12:41 +0000

    Different Methods of DATE

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,16 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate)
+// not able to understand as in miliseconds
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+// Datatype of date is object
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+let mycreatedDate = new Date(2026,3,7)
+console.log(mycreatedDate.toLocaleString())
+
